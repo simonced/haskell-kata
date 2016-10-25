d82186419ac8629447b3f0b34139b282e6b7f785
シモン <simon@azet.jp>
2016-10-25 12:59:55 +0900

    learning Haskell from http://learnyouahaskell.com

---
+++ learnyouhaskell/1_baby.hs
@@ -0,0 +1,13 @@
+doubleMe x = x * 2
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+    then x
+    else x * 2
+
+-- further reading:
+-- http://learnyouahaskell.com/starting-out
+-- An intro to lists
+
+-- vim: set expandtab
