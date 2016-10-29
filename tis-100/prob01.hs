0fd86396631d842f5cb0be77e727b5d0addef8a0
cedric simon <simonced@gmail.com>
2016-10-29 09:48:25 +0900

    improve list types, check function and checkall building

--- tis-100/prob01.hs
+++ tis-100/prob01.hs
@@ -1,29 +1,35 @@
+
 -- problem description:
 -- 2 inputs A and B
 -- 2 outputs A and B
 -- each input data has to be copied to the corresponding output
 
 -- inputs
-inA = [4, 5, -1, 9, 8, 5, 6]
-inB = [2, -6, 8, 7, 11, 43, 1]
+inA = [4, 5, -1, 9, 8, 5, 6]::[Int]
+inB = [2, -6, 8, 7, 11, 43, 1]::[Int]
 
 -- expected outputs
-outA = [4, 5, -1, 9, 8, 5, 6]
-outB = [2, -6, 8, 7, 11, 43, 1]
+outA = [4, 5, -1, 9, 8, 5, 6]::[Int]
+outB = [2, -6, 8, 7, 11, 43, 1]::[Int]
 -- for that simple problem, they are identical
 
 -- process function
-process :: Integer -> Integer
+process :: Int -> Int
 process x = x
 -- simply returns the same parameter
 
 -- output check function
-check :: Integer -> Integer -> (Integer, Integer, Bool)
-check i o = (i, process i, o == process i)
+check :: Int -> Int -> (Int, Int, Bool)
+check i o = (i, result, o == result)
+    where result = process i
 
-checkAll :: [Integer] -> [Integer] -> [(Integer, Integer, Bool)]
+checkAll :: [Int] -> [Int] -> [(Int, Int, Bool)]
 checkAll [] [] = []
-checkAll (i:is) (o:os) = [check i o] ++ checkAll is os
+checkAll (i:is) (o:os) = (check i o):checkAll is os
+
+run = do
+    print (checkAll inA outA)
+    print (checkAll inB outB)
 
 -- then testing with 
 -- checkAll inA ouA
