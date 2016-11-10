456af9499de89e4ca4aea489de5ecec56c387470
シモン <simon@azet.jp>
2016-11-10 13:28:17 +0900

    start of regex and read mail content

--- email-parser/email-parser.hs
+++ email-parser/email-parser.hs
@@ -1,5 +1,11 @@
 -- will turn into a module once it's working
 
+-- package: http://hackage.haskell.org/package/regex-posix
+-- installation intructions "cabal install regex-posix"
+-- if failed but finally worked. problem with firewall!
+
+import Text.Regex.Posix
+
 -- basic data structure
 data MailData = MailData {
             fromEmail :: String
@@ -10,21 +16,23 @@ data MailData = MailData {
 
 
 -- this should open the file and extract the data we need
-parseEmail :: String -> MailData
-parseEmail mailfile = MailData {
-            fromEmail = "fromemail@todo"
+parseEmail :: String -> IO MailData
+parseEmail mailfile = do
+        filecontent <- readFile mailfile
+        return MailData {
+            fromEmail = searchFromIn filecontent
             ,title = ""
             ,spamScore = 4.3
             ,spamFlag = False
             }
-            -- TODO real logic
 
+
+searchFromIn :: String -> String
+searchFromIn content = content =~ "From:.*<(.*)>" :: String
 
 -- for now, we try our function
 main = do
 
-    let mymaildata = parseEmail "emails\\Javaプログラマーに朗報！プログラミングスキル定着に最適「プログラマー早期育成ドリル」_ウェアラブル機器活用最前線【DIP記事】.eml"
+    parseEmail "emails\\Javaプログラマーに朗報！プログラミングスキル定着に最適「プログラマー早期育成ドリル」_ウェアラブル機器活用最前線【DIP記事】.eml"
 
-    -- for testing
-    print mymaildata
 
