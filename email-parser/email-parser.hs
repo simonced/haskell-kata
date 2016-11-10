cbbfd102ae3861be41ef14fa75d2c215ee05fb00
cedric simon <simonced@gmail.com>
2016-11-11 08:27:50 +0900

    change smaple email + add spam score parsing

--- email-parser/email-parser.hs
+++ email-parser/email-parser.hs
@@ -9,7 +9,7 @@ import Text.Regex.Posix
 -- basic data structure
 data MailData = MailData {
             fromEmail :: String
-            ,title :: String
+            ,subject :: String
             ,spamScore :: Float
             ,spamFlag :: Bool
             } deriving (Show)
@@ -21,20 +21,22 @@ parseEmail mailfile = do
         filecontent <- readFile mailfile
         return MailData {
             fromEmail = searchFromIn filecontent
-            ,title = ""
-            ,spamScore = 4.3
+            ,subject = ""
+            ,spamScore = searchSpamScore filecontent
             ,spamFlag = False
             }
+-- TODO subject and spamFlag
 
 
 searchFromIn :: String -> String
-searchFromIn content = content =~ "From:.*<(.*)>" :: String
--- I found a good solution to get only the email:
--- head ("From: longstringofdoom! <simon@test.com>" =~ "From:.*<(.*)>" :: [[String]]) !! 1
+searchFromIn content = head(content =~ "^From:.*<(.*)>" :: [[String]]) !! 1
+
+
+searchSpamScore :: String -> Float
+searchSpamScore content = read (head (content =~ "score=([0-9.]+)" :: [[String]]) !! 1) :: Float
+
 
 -- for now, we try our function
-main = do
-
-    parseEmail "emails\\Javaプログラマーに朗報！プログラミングスキル定着に最適「プログラマー早期育成ドリル」_ウェアラブル機器活用最前線【DIP記事】.eml"
+main = parseEmail "emails\\All-Sport Cutting-Edge Headlight.  Great Holiday Gift. 75% OFF TODAY..eml"
 
 
