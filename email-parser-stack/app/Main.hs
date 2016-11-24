cd3f73406390922c7edc6de9236c3fe7123a6195
シモン <simon@azet.jp>
2016-11-24 12:46:27 +0900

    make main use the new functions, output to finish

--- email-parser-stack/app/Main.hs
+++ email-parser-stack/app/Main.hs
@@ -2,22 +2,36 @@ module Main where
 
 import Lib
 
--- modules I need
---import Regex.Posix
---import System.Directory
+lookupFolder = "emails"
+
+-- ======================================================================
 
 
--- simple tools to test in ghci
--- this function simply allows me to get the MailData I need
--- I'll use it to test other functions
-run :: IO [MailData]
-run = do
-        emails <- makeEmailsList "emails"
-        results <- parseEmails emails
-        return (results)
+--                  _             _ _              __                      
+--   ___ ___  _ __ | |_ _ __ ___ | | | ___ _ __   / _|_   _ _ __   ___ ___ 
+--  / __/ _ \| '_ \| __| '__/ _ \| | |/ _ \ '__| | |_| | | | '_ \ / __/ __|
+-- | (_| (_) | | | | |_| | | (_) | | |  __/ |    |  _| |_| | | | | (__\__ \
+--  \___\___/|_| |_|\__|_|  \___/|_|_|\___|_|    |_|  \__,_|_| |_|\___|___/
+--                                                                         
+
+
+displayFromStats :: [MailData] -> IO ()
+displayFromStats emailsData_ = do
+        -- TODO make a better formated listing
+        print $ groupUniques . compareAllList $ [fromEmail x | x <- emailsData_]
+
+
+
+--                  _       
+--  _ __ ___   __ _(_)_ __  
+-- | '_ ` _ \ / _` | | '_ \ 
+-- | | | | | | (_| | | | | |
+-- |_| |_| |_|\__,_|_|_| |_|
+--                          
 
 
 main :: IO ()
 main = do
-        d <- run
-        print d
+        emailsFiles <- makeEmailsList lookupFolder
+        emailsData <- parseEmails emailsFiles
+        displayFromStats emailsData
