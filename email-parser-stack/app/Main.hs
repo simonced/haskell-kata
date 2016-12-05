2e699b3e674936f7a3ad4dd3bd3947536c643e20
シモン <simon@azet.jp>
2016-12-05 12:59:07 +0900

    start using threepenny for the front (only sample for now)

--- email-parser-stack/app/Main.hs
+++ email-parser-stack/app/Main.hs
@@ -3,6 +3,14 @@ module Main where
 import Lib
 import qualified Data.Map as M
 
+import Control.Monad (void)
+import Data.Maybe
+import Text.Printf
+import Safe          (readMay)
+
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
 lookupFolder = "emails"
 
 -- ======================================================================
@@ -37,6 +45,8 @@ padLeft size text
     where missing = size - (length text)
 
 
+
+
 --                  _       
 --  _ __ ___   __ _(_)_ __  
 -- | '_ ` _ \ / _` | | '_ \ 
@@ -45,10 +55,44 @@ padLeft size text
 --                          
 
 
-main :: IO ()
-main = do
-        emailsFiles <- makeEmailsList lookupFolder
-        emailsData <- parseEmails emailsFiles
-        let emailsOnly = [fromEmail x | x <- emailsData]
-        displayEmailsOnly emailsOnly
-        displayFromStats emailsOnly
+--main :: IO ()
+--main = do
+--        emailsFiles <- makeEmailsList lookupFolder
+--        emailsData <- parseEmails emailsFiles
+--        let emailsOnly = [fromEmail x | x <- emailsData]
+--        displayEmailsOnly emailsOnly
+--        displayFromStats emailsOnly
+
+-- threepenny version (start)
+main = startGUI defaultConfig setup
+
+{-----------------------------------------------------------------------------
+    Main
+------------------------------------------------------------------------------}
+
+-- for now, copy-pasted sample
+setup :: Window -> UI ()
+setup window = void $ do
+    return window # set title "Currency Converter"
+
+    dollar <- UI.input
+    euro   <- UI.input
+    
+    getBody window #+ [
+            column [
+                grid [[string "Dollar:", element dollar]
+                     ,[string "Euro:"  , element euro  ]]
+            , string "Amounts update while typing."
+            ]]
+
+    euroIn   <- stepper "0" $ UI.valueChange euro
+    dollarIn <- stepper "0" $ UI.valueChange dollar
+    let
+        rate = 0.7 :: Double
+        withString f = maybe "-" (printf "%.2f") . fmap f . readMay
+    
+        dollarOut = withString (/ rate) <$> euroIn
+        euroOut   = withString (* rate) <$> dollarIn
+    
+    element euro   # sink value euroOut
+    element dollar # sink value dollarOut
